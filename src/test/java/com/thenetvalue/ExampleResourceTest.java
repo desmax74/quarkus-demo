0ccb2a14f53f68ed2b70e402f2ad13ca15b3def2
Max <desmax74@yahoo.it>
2019-12-04 19:19:46 +0100

    fix test

--- src/test/java/com/thenetvalue/ExampleResourceTest.java
+++ src/test/java/com/thenetvalue/ExampleResourceTest.java
@@ -12,7 +12,7 @@ public class ExampleResourceTest {
     @Test
     public void testHelloEndpoint() {
         given()
-          .when().get("/hello")
+          .when().get("/welcome")
           .then()
              .statusCode(200)
              .body(is("hello"));
